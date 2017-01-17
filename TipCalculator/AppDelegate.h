949d79333483fc1cd33cdce8d1ef5a8193c82354
Steve Carlson <yasteve@yahoo-inc.com>
2017-01-17 15:38:43 -0800

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by  Steve Carlson (Media Engineering) on 1/17/17.
+//  Copyright © 2017 Steve Carlson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by  Steve Carlson (Media Engineering) on 1/17/17.
+//  Copyright © 2017 Steve Carlson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
